d58ed34d8d9118d771258898cd6fc5f06011998b
huzeyfetas <huzolagaluga@gmail.com>
2020-05-30 13:07:26 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>app2</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
